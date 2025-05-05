2966bdf4381c2601d1a2807438f10d75035481e7
viniciuserreira <vinicius.fsouza@eaportal.org>
2025-05-05 06:48:22 +0000

    resposta do exercicio um

---
+++ pages/extremamente basico/extremamente_facil.js
@@ -0,0 +1,10 @@
+function somar(valor1, valor2){
+    return valor1 + valor2
+}
+
+let teste1 = somar(10, 9)
+let teste2 = somar(-10, 4)
+let teste3 = somar(15, -7)
+
+
+console.log(`X1 = ${teste1}\nX2 = ${teste2}\nX3 = ${teste3}`)
